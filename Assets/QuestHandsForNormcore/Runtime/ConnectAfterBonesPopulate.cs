d9d730c47de682d926bde35b3315686e0b100078
Andy <andy@radialgames.com>
2020-08-18 12:15:46 -0700

    Bones dynamically instantiate from OVR now, so this delays connecting until the bones are ready

---
+++ Assets/QuestHandsForNormcore/Runtime/ConnectAfterBonesPopulate.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using Normal.Realtime;
+using UnityEngine;
+
+namespace absurdjoy
+{
+    public class ConnectAfterBonesPopulate : MonoBehaviour
+    {
+        public string roomName;
+
+        IEnumerator Start()
+        {
+            var skeleton = GetComponent<SpawnPlayerAvatar>().localLeftHandReference.GetComponentInChildren<OVRSkeleton>();
+            while (skeleton.Bones.Count == 0)
+            {
+                yield return null;
+            }
+
+            skeleton = GetComponent<SpawnPlayerAvatar>().localRightHandReference.GetComponentInChildren<OVRSkeleton>();
+            while (skeleton.Bones.Count == 0)
+            {
+                yield return null;
+            }
+
+            var realtime = GetComponent<Realtime>();
+            realtime.Connect(roomName);
+        }
+    }
+}
